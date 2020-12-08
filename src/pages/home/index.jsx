f4c21472b39af9b55ce1ed129018a036e6a950ae
userLi <lmj9lmj@163.com>
2020-12-08 18:37:32 +0800

    封装头部

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+
 import './styles.less'
+import HeaderN from '@/components/HeaderN'
 
-function Home (props) {
-
+function Home(props) {
+  const r = () => {
+    console.log('r');
+  }
   return (
     <div className="pages-home">
-      
+      <HeaderN
+        isSearch={true}
+        lef="ele.me"
+        cen="天津"
+        rig=""
+        rigOnClick={r}
+      />
     </div>
   )
 }
