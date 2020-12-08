0c68f26c362cd0d16f927fac5d593ae67fae60c4
anhang-cmyk <758409262@qq.com>
2020-12-08 19:26:17 +0800

    '配置路由'

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,12 @@
 import React from 'react'
-import { BrowserRouter, Switch, Route, Link, NavLink } from 'react-router-dom'
-import { renderRoutes } from 'react-router-config'
+import { Switch, Route } from 'react-router-dom'
 import { Home } from '@/router/componens'
-
-function Root ({ route }) {
-  return renderRoutes(route.routes)
-}
-
-const routes = [
-  {
-    component: Root,
-    routes: [
-      {
-        path: '/',
-        component: Home,
-      },
-    ]
-  }
-]
-
-export default function Router () {
+export default function Router() {
   return (
-    <BrowserRouter>
-      <>
-        {/* <h1><Link to="/login">login</Link></h1>
-        <h1><Link to="/forms">forms</Link></h1>
-        <h1><Link to="/">home</Link></h1> */}
-        {renderRoutes(routes)}
-      </>
-    </BrowserRouter>
+    <div>
+      <Switch>
+        <Route path="/home" component={Home} />
+      </Switch>
+    </div>
   )
 }
