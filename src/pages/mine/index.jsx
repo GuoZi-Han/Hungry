ac1a506b97f9e4922c4ce5c60aec76fede8a38cc
guocchao <1084062798@qq.com>
2020-12-09 16:05:58 +0800

    '我的头部样式'

--- src/pages/mine/index.jsx
+++ src/pages/mine/index.jsx
@@ -1,21 +1,43 @@
-import React from 'react'
-
+import React, { useState } from 'react'
 
 import './styles.less'
 import HeaderN from '@/components/HeaderN'
 import FooterN from '@/components/FooterN'
 
-function mine(props) {
-
+function Mine(props) {
+  const [headline, setHeadline] = useState('我的')
+  const [headportrait, setHeadportrait] = useState('/img/headportrait.jpg')
   return (
     <div className="lmj_home">
-      <HeaderN />
-      <section>
-        我的页
+      <HeaderN cen={headline} />
+      <section className="gc_section">
+        <div>
+            <div>
+                <div>
+                  <img src={headportrait}/>
+                </div>
+            </div>
+            <div>
+                <span></span>
+                <span></span>
+            </div>
+            <div>
+
+            </div>
+        </div>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
       </section>
       <FooterN />
     </div>
   )
 }
 
-export default mine
+export default Mine
