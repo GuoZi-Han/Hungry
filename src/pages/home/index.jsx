5be0c538198467b6435b87d8bc3502f07bedc7fe
userLi <lmj9lmj@163.com>
2020-12-09 08:42:03 +0800

    轮播图

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -18,34 +18,6 @@ function Home(props) {
               <dt><img src="https://fuss10.elemecdn.com/2/35/696aa5cf9820adada9b11a3d14bf5jpeg.jpeg" alt="" /></dt>
               <dd>甜品饮品</dd>
             </dl>
-            <dl>
-              <dt><img src="https://fuss10.elemecdn.com/2/35/696aa5cf9820adada9b11a3d14bf5jpeg.jpeg" alt="" /></dt>
-              <dd>甜品饮品</dd>
-            </dl>
-            <dl>
-              <dt><img src="https://fuss10.elemecdn.com/2/35/696aa5cf9820adada9b11a3d14bf5jpeg.jpeg" alt="" /></dt>
-              <dd>美食</dd>
-            </dl>
-            <dl>
-              <dt><img src="https://fuss10.elemecdn.com/2/35/696aa5cf9820adada9b11a3d14bf5jpeg.jpeg" alt="" /></dt>
-              <dd>简餐</dd>
-            </dl>
-            <dl>
-              <dt><img src="https://fuss10.elemecdn.com/2/35/696aa5cf9820adada9b11a3d14bf5jpeg.jpeg" alt="" /></dt>
-              <dd>甜品饮品</dd>
-            </dl>
-            <dl>
-              <dt><img src="https://fuss10.elemecdn.com/2/35/696aa5cf9820adada9b11a3d14bf5jpeg.jpeg" alt="" /></dt>
-              <dd>准时达</dd>
-            </dl>
-            <dl>
-              <dt><img src="https://fuss10.elemecdn.com/2/35/696aa5cf9820adada9b11a3d14bf5jpeg.jpeg" alt="" /></dt>
-              <dd>甜品饮品</dd>
-            </dl>
-            <dl>
-              <dt><img src="https://fuss10.elemecdn.com/2/35/696aa5cf9820adada9b11a3d14bf5jpeg.jpeg" alt="" /></dt>
-              <dd>甜品饮品</dd>
-            </dl>
           </div>
           <div>
             <h3 style={contentStyle}>2</h3>
@@ -57,9 +29,8 @@ function Home(props) {
   )
 }
 const contentStyle = {
-  height: '160px',
+  height: '3.5rem',
   color: '#000',
-  lineHeight: '160px',
   textAlign: 'center',
   background: '#364d79',
 };
