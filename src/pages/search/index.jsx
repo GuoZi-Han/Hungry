0ca733b5de2c297084eff4dc6d2624454d8872ec
GuoZi-Han <2942654095@qq.com>
2020-12-09 18:41:21 +0800

    搜索页布局

--- src/pages/search/index.jsx
+++ src/pages/search/index.jsx
@@ -1,16 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './styles.less'
 import HeaderN from '@/components/HeaderN'
 import FooterN from '@/components/FooterN'
+import { Input, Button } from 'antd'
+import { CloseOutlined } from '@ant-design/icons'
 
 function Search(props) {
+
+  const [isBool, setIsBool] = useState('none')
+  const 
 
   return (
     <div className="home lmj_home">
       <HeaderN />
-      <section>
-        搜索页
+      <section className = 'gzh_section'>
+        <div className = 'gzh_search_div'>
+          <Input placeholder = '请输入商家或美食名称'/>
+          <Button type = 'primary' className = 'gzh_search_button'>提交</Button>
+        </div>
+        <div className = 'gzh-title' style = {{ display: isBool }}>
+          <h4 className = 'gzh-font'>搜索历史</h4>
+          <ul>
+            <li>
+              <h6>效果演示</h6>
+              <CloseOutlined />
+            </li>
+          </ul>
+          <p>清空搜索历史</p>
+        </div>
       </section>
       <FooterN />
     </div>
