3122afdf72f939b5f8f34502c2161f690e3ba946
GuoZi-Han <2942654095@qq.com>
2020-12-09 15:59:08 +0800

    完成

--- src/index.js
+++ src/index.js
@@ -4,16 +4,13 @@ import { Provider } from 'react-redux'
 import { persistStore } from 'redux-persist'
 import { BrowserRouter } from 'react-router-dom'
 import { PersistGate } from 'redux-persist/lib/integration/react'
+import { someDayAdminRegNum } from '@/utils/interFaces'
 import store from './store'
 import Rouer from '@/router'
 import '@/styles/rem'
 import '@/styles/reset.css'
 import '@/styles/index.less'
-import { someDayAdminRegNum } from '@/utils/interFaces'
 
-someDayAdminRegNum('2017-05-29').then(res => {
-  console.log(res)
-})
 
 ReactDOM.render(
   <Provider store={store}>
