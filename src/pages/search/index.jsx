4dc12922f5e074ec9fd0e176804c7a76cd80ba55
GuoZi-Han <2942654095@qq.com>
2020-12-10 20:38:07 +0800

    搜索2

--- src/pages/search/index.jsx
+++ src/pages/search/index.jsx
@@ -5,6 +5,7 @@ import HeaderN from '@/components/HeaderN'
 import FooterN from '@/components/FooterN'
 import { Input, Button } from 'antd'
 import { CloseOutlined } from '@ant-design/icons'
+import Icon from '@ant-design/icons';
 import { connect } from 'react-redux'
 // 搜索action
 import searchActions from '@/actions/search'
@@ -22,24 +23,61 @@ export default connect(
 )(Search)
 
 function Search(props) {
-
+  // 定义数据
   const [isBool, setIsBool] = useState('none')
+  const [gzh_bool, setGhz_bool] = useState('none')
   const [value, setValue] = useState()
   const [data, setData] = useState([])
-
+  const [history_data, setHistory_data] = useState([])
+  //  调用生命周期 监听数据
   useEffect(() => {
-    // data.length !== 0 ? setIsBool('block') : setIsBool('none')
-  }, [data])
+    // 判断数组长度
+    data.length !== 0 ? setGhz_bool('block') : setGhz_bool('none')
+    //判断数组长度以及value值是否符合要求
+    if (data.length && value === '') {
+      // 设置显示隐藏
+      setIsBool('block')
+      setGhz_bool('none')
+    } else {
+      // 隐藏盒子
+      setIsBool('none')
+    }
+  }, [data, value])
 
+  // 输入框onchange事件
   const searchInput = (e) => {
     setValue(e.target.value)
+    // 值为空 清除数据
+    if(value === ''){
+      setData([])
+    }
   }
-
+  // 点击提交按钮事件
   const searchClick = ()  => {
+    if (sessionStorage.getItem('historyArray')) {
+      history_data.push(value)
+      console.log(history_data)
+      sessionStorage.setItem('historyArray',JSON.stringify(history_data))
+    } else {
+      const arr = sessionStorage.getItem('historyArray')
+      console.log(arr)
+      const historyBool = arr.some(v => {
+        return v === value
+      })
+      if (!historyBool) {
+        arr.push(value)
+        sessionStorage.setItem('historyArray',arr)
+      }
+    }
+    // 请求数据 
     props.searchShop({ geohash: '31.22967,121.4762', keyword: value})
       .then(res => {
         console.log(res.payload.data)
-        setData(res.payload.data)
+        if (res.payload.data.length > 0) {
+          setData(res.payload.data)
+        } else {
+          setData([])
+        }
       })
   }
 
@@ -48,9 +86,10 @@ function Search(props) {
       <HeaderN />
       <section className = 'gzh_section'>
         <div className = 'gzh_search_div'>
-          <Input 
-            placeholder = '请输入商家或美食名称' 
-            value = { value } 
+          <Input
+            placeholder="请输入商家或美食名称"
+            allowClear 
+            value = { value }
             onChange = { searchInput } 
           />
           <Button 
@@ -61,16 +100,45 @@ function Search(props) {
             提交
           </Button>
         </div>
-        <div className = 'gzh_search_list'>
+        <div className = 'gzh_search_list' style = {{ display: gzh_bool }} >
           <h4>商家</h4>
+          <ul className = 'gzh_show_list'>
+            {
+              data.map((v, i) => {
+                return (
+                  <li key = { i }>
+                    <dl>
+                      <dt><img src = { `//elm.cangdu.org/img/${ v.image_path }` } /></dt>
+                      <dd>
+                        <p>
+                          <font>{v.name}</font>
+                          <font className = 'svg'>
+                            <span style = {{fontSize: '0.1rem'}}>支付</span>
+                          </font>
+                        </p>
+                        <p>月售<span>{ v.recent_order_num }</span>单</p>
+                        <p> <span>{ v.float_minimum_order_amount }</span> 元起送 <span>/</span> 距离{ v.distance }</p>
+                      </dd>
+                    </dl>
+                  </li>
+                )
+              })
+            }
+          </ul>
         </div>
         <div className = 'gzh-title' style = {{ display: isBool }}>
           <h4 className = 'gzh-font'>搜索历史</h4>
           <ul>
-            <li>
-              <h6>效果演示</h6>
-              <CloseOutlined />
-            </li>
+            {
+              JSON.parse(sessionStorage.getItem('historyArray')).map((v, i) => {
+                return (
+                  <li key = { i }>
+                    <h6>{ v }</h6>
+                    <CloseOutlined />
+                  </li>
+                )
+              })
+            }
           </ul>
           <p>清空搜索历史</p>
         </div>
