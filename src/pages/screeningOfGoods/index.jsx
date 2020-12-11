d2c5adad790a5b5139359e44126e3dca1f3af86e
userLi <lmj9lmj@163.com>
2020-12-11 18:52:03 +0800

    ......

--- src/pages/screeningOfGoods/index.jsx
+++ src/pages/screeningOfGoods/index.jsx
@@ -9,6 +9,7 @@ import {
 } from '@/constants/actionTypes'
 import HeaderN from '@/components/HeaderN'
 import Shopdl from '@/components/Shopdl'
+import ModalN from '@/components/ModalN'
 
 export default connect(
   (state) => ({ shopData: state.home.shopData }),
@@ -21,9 +22,11 @@ function ScreeningOfGoods(props) {
     decodeURIComponent(props.location.search).split('?')[1].split('=')[1] ?
       decodeURIComponent(props.location.search).split('?')[1].split('=')[1] : ""
   )
+  const [typeTxt, setTypeTxt] = useState(type)
   const [bool1, setBool1] = useState("")
   const [bool2, setBool2] = useState("")
   const [bool3, setBool3] = useState("")
+  // const [visible, setVisible] = useState(false)
   useEffect(() => {
     if (!props.shopData.length) {
       (async () => { await props.getShopList({ latitude: 31.22967, longitude: 121.4762 }) })()
@@ -35,20 +38,26 @@ function ScreeningOfGoods(props) {
     const data = [...props.shopData]
     return data
   }
+  // 导航三角动画和模态框是否显示
   const navFun = (val) => {
     if (val === 1) {
       if (bool1 === "" || bool1 === "2") {
+        setTypeTxt('分类')
         setBool1("1")
         if (bool2 === "1") setBool2("2")
         if (bool3 === "1") setBool3("2")
       } else {
         setBool1("2")
+        setTypeTxt(type)
       }
       return
     } else if (val === 2) {
       if (bool2 === "" || bool2 === "2") {
         setBool2("1")
-        if (bool1 === "1") setBool1("2")
+        if (bool1 === "1") {
+          setBool1("2")
+          setTypeTxt(type)
+        }
         if (bool3 === "1") setBool3("2")
       } else {
         setBool2("2")
@@ -57,7 +66,10 @@ function ScreeningOfGoods(props) {
     } else {
       if (bool3 === "" || bool3 === "2") {
         setBool3("1")
-        if (bool1 === "1") setBool1("2")
+        if (bool1 === "1") {
+          setBool1("2")
+          setTypeTxt(type)
+        }
         if (bool2 === "1") setBool2("2")
       } else {
         setBool3("2")
@@ -66,10 +78,10 @@ function ScreeningOfGoods(props) {
   }
   return (
     <div className="lmj_ScreeningOfGoods home">
-      <HeaderN cen={type} />
-      <div className="lmj_NavFun">
+      <HeaderN cen={type} className="topF" />
+      <div className="lmj_NavFun topF">
         <div onClick={() => navFun(1)} className={"navOn" + bool1}>
-          {type}
+          {typeTxt}
           <CaretDownOutlined />
         </div>
         <div onClick={() => navFun(2)} className={"navOn" + bool2}>
@@ -81,8 +93,8 @@ function ScreeningOfGoods(props) {
           <CaretDownOutlined />
         </div>
       </div>
-      <section>
-        <div>
+      <section className="lmj_ModalSec">
+        <div className="bbb">
           {
             shopDataFilter().length ? shopDataFilter().map((v, i) => {
               return (
@@ -91,6 +103,14 @@ function ScreeningOfGoods(props) {
             }) : "暂无"
           }
         </div>
+        {/* <ModalN>
+          <p>123456</p>
+          <p>123456</p>
+          <p>123456</p>
+          <p>123456</p>
+          <p>123456</p>
+          <p>123456</p>
+        </ModalN> */}
       </section>
     </div>
   )
